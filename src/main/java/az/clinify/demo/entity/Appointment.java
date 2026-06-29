cf7214cf2d11f7f482bfcba7565a97c0f43e1a26
Asiman Mirzaliyev <asiman.mirzaliyev2023@khazar.org>
2026-06-29 21:13:24 +0400

    feat: add relations -- doctor and patient

--- src/main/java/az/clinify/demo/entity/Appointment.java
+++ src/main/java/az/clinify/demo/entity/Appointment.java
@@ -1,10 +1,22 @@
 package az.clinify.demo.entity;
 
 import az.clinify.demo.entity.BaseEntity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "appointments")
 public class Appointment extends BaseEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "patient_id", nullable = false)
+    private User patient;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "doctor_id", nullable = false)
+    private DoctorProfile doctor;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "created_by_id")
+    private User createdBy;
+
 }

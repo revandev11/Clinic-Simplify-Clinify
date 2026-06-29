caa858a4bc94112be242d155c76c240919286022
Asiman Mirzaliyev <asiman.mirzaliyev2023@khazar.org>
2026-06-29 21:25:58 +0400

    optimize: Comments are added and lomboks

--- src/main/java/az/clinify/demo/entity/Appointment.java
+++ src/main/java/az/clinify/demo/entity/Appointment.java
@@ -4,12 +4,20 @@ import az.clinify.demo.entity.BaseEntity;
 import az.clinify.demo.enums.AppointmentStatus;
 import az.clinify.demo.enums.AppointmentType;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 
 //This Entity class are added for booking APppointment which will happen between doctors and patient
 //It extends BaseEntity for id , createdAt , updatedAt fields
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "appointments")
 public class Appointment extends BaseEntity {
@@ -36,12 +44,14 @@ public class Appointment extends BaseEntity {
     @Column(nullable = false, length = 30)
     private AppointmentStatus status = AppointmentStatus.REQUESTED;
 
+    //Appointment start and end time
     @Column(name = "start_time", nullable = false)
     private LocalDateTime startTime;
 
     @Column(name = "end_time", nullable = false)
     private LocalDateTime endTime;
 
+    //This field can be null, this is not mandatory
     @Column(columnDefinition = "TEXT")
     private String reason;
 
--- src/main/java/az/clinify/demo/entity/Department.java
+++ src/main/java/az/clinify/demo/entity/Department.java
@@ -1,8 +1,19 @@
 package az.clinify.demo.entity;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "appointments")
 public class Department extends BaseEntity{
 
     @Column(nullable = false, unique = true, length = 150)

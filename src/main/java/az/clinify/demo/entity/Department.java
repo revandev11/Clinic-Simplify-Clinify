5f3ac3f32e3634347709dac702e2ee0252ffb3ee
Nuray Taghiyeva <tagiyevanuray35@gmail.com>
2026-06-29 19:29:04 +0400

    feat: create Department entity

---
+++ src/main/java/az/clinify/demo/entity/Department.java
@@ -0,0 +1,15 @@
+package az.clinify.demo.entity;
+
+import jakarta.persistence.Column;
+
+public class Department extends BaseEntity{
+
+    @Column(nullable = false, unique = true, length = 150)
+    private String name;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @Column(nullable = false)
+    private Boolean active = true;
+}
